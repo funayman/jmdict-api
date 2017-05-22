be7b241d363426216c4114388d9d616dee3fa54c
Dave Derderian <funayman@gmail.com>
2017-05-22 20:43:58 +0900

    base complete

--- main.go
+++ main.go
@@ -10,16 +10,31 @@ import (
 
 func main() {
 	//get the file
-	data, err := os.Open("./data/test-data.xml")
+	data, err := os.Open("./data/JMdict_e")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer data.Close()
 
-	err = parser.Parse(data)
+	words, err := parser.LoadJMDict(data)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Current count: ", parser.Count())
+	fmt.Println("Current count: ", len(words))
+
+	//kanjidic2
+	kanjiFile, err := os.Open("./data/kanjidic2.xml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer data.Close()
+
+	kanji, err := parser.LoadKanjiDic2(kanjiFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Len of Kanji: %d\n", len(kanji))
+	fmt.Printf("%+v\n", kanji[100])
 }
